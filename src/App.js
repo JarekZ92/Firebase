d102f93a082b6e7c971dd8ffdd41c36c251ce34b
Jarek <z.jarek92@gmail.com>
2018-08-08 10:11:16 +0200

    added new componets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import UserList from './UserList'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <UserList />
       </div>
     );
   }
