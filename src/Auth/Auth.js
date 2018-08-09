da2b7a54065dea3688d9263b6a8ee106ae31c975
Jarek <z.jarek92@gmail.com>
2018-08-09 14:26:39 +0200

    Hardcoded login button

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
-const isLogedIn = false
+import LogInForms from './LogInForms';
 
-const Auth = (props) => (
-    <div>
-        {
-            isLogedIn ?
-                props.children
-                :
-                'Not loged in!!'
-        }
-    </div>
-)
+class Auth extends React.Component {
+    state ={
+        isLogedIn: false
+    }
+
+    onLogInClickHandler = () => {
+        this.setState({
+            isLogedIn: true
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.isLogedIn ?
+                        this.props.children
+                        :
+                        <LogInForms 
+                        onLogInClickHandler={this.onLogInClickHandler}
+                        />
+                }
+            </div>
+        )
+    }
+}
 
 export default Auth
