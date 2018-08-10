e7eff6e6fe2b4fc1196af5bfad6f49946c5d8ee1
Jarek <z.jarek92@gmail.com>
2018-08-10 09:15:20 +0200

    passing props into LogInByGoogle

--- src/Auth/LogInForms.js
+++ src/Auth/LogInForms.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
+import LogInByGoogle from './LoginByGoogle'
+
 const LogInForms = (props) => (
     <div>
-        <button
-            onClick={props.onLogInClickHandler}
-        >
-            Login!
-        </button>
+        <LogInByGoogle
+            onLogInClickHandler={props.onLogInClickHandler}
+        />
     </div>
 )
 
